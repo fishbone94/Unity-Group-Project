bda7b05db8e014369cd2742ec94af6c00289e49b
ncboodah <nanney.boodah@hotmail.com>
2014-11-04 22:58:59 -0800

    Created MatchObject for the objects
    Created Matches to run stuff using objects from MatchObject

---
+++ Assets/MatchObject.cs
@@ -0,0 +1,66 @@
+﻿public class MatchObject
+{
+		private static MatchObject _matchSingleton {
+				get;
+				set;
+		}
+
+		public static MatchObject MatchSingleton {
+				get {
+						return _matchSingleton ?? (_matchSingleton = new MatchObject ());
+				}
+		}
+
+		private const int STARTING_NUMBER_MATCHES = 10;
+		private const int MAXIMUM_BRIGHTNESS = 1000;
+		// Rate of match brightness decrease per frame
+		private const int MATCH_BRIGHTNESS_DECREASE_RATE = 5;
+
+		private MatchObject(){
+			NumberOfMatches = STARTING_NUMBER_MATCHES;
+		}
+
+		public bool IsLit {
+				get {
+						if (MatchBrightness > 0)
+								return true;
+						else
+								return false;
+				}
+		}
+	
+		public int MatchBrightness {
+				get;
+				set;
+		}
+
+		public int NumberOfMatches {
+				get;
+				set;
+		}
+
+		public int IncreaseMatches (int increase)
+		{
+				NumberOfMatches += increase;
+				if (NumberOfMatches < 0)
+						NumberOfMatches = 0;
+				return NumberOfMatches;
+		}
+
+		public void DecreaseMatchBrightness ()
+		{
+				MatchBrightness -= MATCH_BRIGHTNESS_DECREASE_RATE;
+				if (MatchBrightness <= 0)
+						NumberOfMatches--;
+		}
+
+		public bool LightNewMatch ()
+		{
+				if (NumberOfMatches == 0 || MatchBrightness >= MAXIMUM_BRIGHTNESS/2)
+						return false;
+				NumberOfMatches--;
+				MatchBrightness = MAXIMUM_BRIGHTNESS;
+				return true;
+		}
+
+}
---
+++ Assets/Matches.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Matches : MonoBehaviour
+{
+
+		private MatchObject _matchSingleton;
+
+		// Use this for initialization
+		void Start ()
+		{
+				_matchSingleton = MatchObject.MatchSingleton;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (_matchSingleton.IsLit) {
+						_matchSingleton.DecreaseMatchBrightness ();
+				}
+		}
+}
