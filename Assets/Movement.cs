b12ade0e809e661099d8b3d7558043eac598a8e6
Edward Chao <edwardchao@live.com>
2014-11-06 00:32:11 -0800

    wOw

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,51 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class Movement : MonoBehaviour {
-
-	public float playerSpeed = 10.0f;
-	public float jumpForce = 350.0f;
-	[HideInInspector]
-	public bool jump = false;
-
-	private Transform groundCheck;			// A position marking where to check if the player is grounded.
-	private bool grounded = false;			// Whether or not the player is grounded.
-
-
+	public bool grounded = true;
+	public float jumpPower = 150;
+	private bool hasJumped = false;
+	public int playerSpeed = 5;
+	
 	// Use this for initialization
-	void Awake(){
-		// Setting up references.
-		groundCheck = transform.Find("groundCheck");
+	void Start () {
 	}
-	
-	void Update(){
-		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
-		
-		// If the jump button is pressed and the player is grounded then the player should jump.
-		if(Input.GetButtonDown("Jump") && grounded)
-			jump = true;
-	}
-
-	void Start(){
-	
-	}
-	
 	// Update is called once per frame
-	void FixedUpdate(){
-	
-		float hValue = Input.GetAxis ("Horizontal");
-
-		rigidbody2D.velocity = new Vector2 (hValue * playerSpeed, rigidbody2D.velocity.y); 
-
-	if(jump){
-			// Add a vertical force to the player.
-			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
-			
-			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
-			jump = false;
+	void Update () {
+		Movement2 ();
+		// Do something
+		if(!grounded && rigidbody2D.velocity.y == 0) {
+			grounded = true;
 		}
-		if (Input.GetButtonDown ("Jump"))
-						rigidbody2D.AddForce (new Vector2 (0, jumpForce));
+		if (Input.GetButtonDown("Jump") && grounded == true) {
+			hasJumped = true;
+		}
+	}
+	void FixedUpdate (){
+		if(hasJumped){
+			rigidbody2D.AddForce(transform.up*jumpPower);
+			grounded = false;
+			hasJumped = false;
+		}
+	}
+	
+	void Movement2()
+	{
+		float hValue = Input.GetAxis ("Horizontal");
+		
+		rigidbody2D.velocity = new Vector2 (hValue * playerSpeed, rigidbody2D.velocity.y);
+		
+		
 	}
 }
