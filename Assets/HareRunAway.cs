3dfddd0cf3e561204343036baf3c9974e0ffac68
Edward Chao <edwardchao@live.com>
2014-11-07 02:31:00 -0800

    FastHare

---
+++ Assets/HareRunAway.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HareRunAway : MonoBehaviour {
+	public float jumpPower = 30;
+	private int hareSpeed = 25;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.tag == "Player"){
+			rigidbody2D.velocity = new Vector2 (hareSpeed,0);
+			Death();
+			}
+	}
+	void Death (){
+			Destroy (gameObject, 1);
+		}
+}
