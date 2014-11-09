fadbab4c9e169121c88b9cfd5cacfafeb3ce6dff
Edward Chao <edwardchao@live.com>
2014-11-09 02:34:51 -0800

    disflippinbunny

--- Assets/HareRunAway.cs
+++ Assets/HareRunAway.cs
@@ -4,23 +4,33 @@ using System.Collections;
 public class HareRunAway : MonoBehaviour {
 	public float jumpPower = 30;
 	private int hareSpeed = 25;
+	private float direction;
+
+	Movement script;
 
 	// Use this for initialization
 	void Start () {
-	
+		GameObject player = GameObject.Find("Player");
+		script = player.GetComponent<Movement> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-	
+		direction = script.someScale;
 	}
 
 
 	void OnCollisionEnter2D(Collision2D col){
 		if(col.gameObject.tag == "Player"){
-			rigidbody2D.velocity = new Vector2 (hareSpeed,0);
-			Death();
+			if(direction > 0){
+				rigidbody2D.velocity = new Vector2 (hareSpeed,0);
+				transform.localScale = new Vector2 (2, transform.localScale.y);
+				Death();
+			}else{
+				rigidbody2D.velocity = new Vector2 (-hareSpeed,0);
+				transform.localScale = new Vector2 (-2, transform.localScale.y);
+				Death();
+			}
 			}
 	}
 	void Death (){
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,7 +6,7 @@ public class Movement : MonoBehaviour {
 	private bool hasJumped = false;
 	private int MAX_PLAYER_SPEED = 5;
 	private float MATCH_LIGHT_INTENSITY_MAX = 8f;
-	private float someScale = 1;
+	public float someScale;
 	private int playerSpeed{
 		get;
 		set;
@@ -20,8 +20,7 @@ public class Movement : MonoBehaviour {
 	void Update () {
 		playerSpeed = MAX_PLAYER_SPEED * MatchObject.MatchSingleton.MatchBrightnessPercentage;
 		light.intensity = (MATCH_LIGHT_INTENSITY_MAX * (float) MatchObject.MatchSingleton.MatchBrightnessPercentage)/100;
-		Movement2 ();
-		// Do something
+			// Do something
 		if(!grounded && rigidbody2D.velocity.y == 0) {
 			grounded = true;
 		}
@@ -30,6 +29,8 @@ public class Movement : MonoBehaviour {
 		}
 }
 	void FixedUpdate (){
+		Movement2 ();
+		Flip ();
 		if(hasJumped){
 			rigidbody2D.AddForce(transform.up*jumpPower);
 			grounded = false;
@@ -39,7 +40,6 @@ public class Movement : MonoBehaviour {
 	
 	void Movement2(){
 		if (grounded) {
-			Flip();
 			float hValue = Input.GetAxis ("Horizontal");
 //			float floatPlayerSpeed = (float) playerSpeed;
 			rigidbody2D.velocity = new Vector2 ((hValue * playerSpeed)/100, rigidbody2D.velocity.y);
@@ -47,9 +47,11 @@ public class Movement : MonoBehaviour {
 	}
 	void Flip(){
 				if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-						transform.localScale = new Vector2 (-someScale, transform.localScale.y);
+						transform.localScale = new Vector2 (-1, transform.localScale.y);
+			someScale = -1;
 				} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
-						transform.localScale = new Vector2 (someScale, transform.localScale.y);
+						transform.localScale = new Vector2 (1, transform.localScale.y);
+			someScale = 1;
 				} else {
 				}
 		}
