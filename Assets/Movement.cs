a3dd7e7bc6daa9f745658bd1983bcfede255cea2
ncboodah <nanney.boodah@hotmail.com>
2014-11-09 23:44:07 -0800

    Updated playerSpeed to gradually decrease to minimum speed once match is out.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,21 +4,23 @@ public class Movement : MonoBehaviour {
 	public bool grounded = true;
 	public float jumpPower = 150;
 	private bool hasJumped = false;
-	private int MAX_PLAYER_SPEED = 5;
+	private int MIN_PLAYER_SPEED = 200;
+	private int MAX_PLAYER_SPEED = 700;
 	private float MATCH_LIGHT_INTENSITY_MAX = 8f;
 	public float someScale;
-	private int playerSpeed{
-		get;
-		set;
+	private int playerSpeed {
+				get;
+				set;
 	}
 	
 	// Use this for initialization
 	void Start () {
 		someScale = transform.localScale.x;
+
 	}
 	// Update is called once per frame
 	void Update () {
-		playerSpeed = MAX_PLAYER_SPEED * MatchObject.MatchSingleton.MatchBrightnessPercentage;
+		UpdatePlayerSpeed ();
 		light.intensity = (MATCH_LIGHT_INTENSITY_MAX * (float) MatchObject.MatchSingleton.MatchBrightnessPercentage)/100;
 			// Do something
 		if(!grounded && rigidbody2D.velocity.y == 0) {
@@ -27,6 +29,8 @@ public class Movement : MonoBehaviour {
 		if (Input.GetButtonDown("Jump") && grounded == true) {
 			hasJumped = true;
 		}
+
+
 }
 	void FixedUpdate (){
 		Movement2 ();
@@ -56,5 +60,13 @@ public class Movement : MonoBehaviour {
 				}
 		}
 
-
+	void UpdatePlayerSpeed(){
+		if (MatchObject.MatchSingleton.MatchBrightness > 0) {
+						playerSpeed = MAX_PLAYER_SPEED - MIN_PLAYER_SPEED;	
+				} else if (MatchObject.MatchSingleton.MatchBrightness == 0 && playerSpeed > MIN_PLAYER_SPEED) {
+						playerSpeed = (int)(playerSpeed - 1);
+				} else
+						playerSpeed = MIN_PLAYER_SPEED;
+	}
+	
 }
